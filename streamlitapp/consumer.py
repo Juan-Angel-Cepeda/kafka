9303cad6253b25595eb1ffd6485ad33a7727263d
angelcepeda <jangelcepeda@protonmail.com>
2023-11-27 14:25:13 -0600

    ya consume

--- streamlitapp/consumer.py
+++ streamlitapp/consumer.py
@@ -5,7 +5,7 @@ class Consumer:
     def __init__(self, topic):
         self.consumer = KafkaConsumer(
             topic,
-            bootstrap_servers=['localhost:9091'],
+            bootstrap_servers=['localhost:9092'],
             auto_offset_reset='earliest',
             enable_auto_commit=True,
             group_id='my-group',
